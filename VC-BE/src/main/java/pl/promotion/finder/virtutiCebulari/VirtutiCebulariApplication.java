48a45a02e7102d62ff3f6ca9412f83844f0329e6
kafarakamil95@gmail.com <kafarakamil95@gmail.com>
2019-07-02 19:07:53 +0200

    initial commit

---
+++ VC-BE/src/main/java/pl/promotion/finder/virtutiCebulari/VirtutiCebulariApplication.java
@@ -0,0 +1,13 @@
+package pl.promotion.finder.virtutiCebulari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtutiCebulariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtutiCebulariApplication.class, args);
+	}
+
+}
