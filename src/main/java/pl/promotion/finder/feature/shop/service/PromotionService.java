f3181529a89c0cdceb440f29cbbdb3bc5c2d3c51
Kamil Kafara <kafarakamil95@gmail.com>
2020-08-29 21:37:18 +0200

    refactoring and testing

--- src/main/java/pl/promotion/finder/feature/shop/service/PromotionService.java
+++ src/main/java/pl/promotion/finder/feature/shop/service/PromotionService.java
@@ -2,6 +2,7 @@ package pl.promotion.finder.feature.shop.service;
 
 import org.springframework.stereotype.Service;
 import pl.promotion.finder.feature.shop.dto.ProductDTO;
+import pl.promotion.finder.feature.slackbot.SlackMessageSender;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -18,8 +19,9 @@ public class PromotionService {
     private final MoreleService moreleService;
     private final XkomService xkomService;
     private final ZadowolenieService zadowolenieService;
+    private final SlackMessageSender slackMessageSender;
 
-    public PromotionService(AltoService altoService, AmsoService amsoService, CarinetService carinetService, CombatService combatService, MoreleService moreleService, XkomService xkomService, ZadowolenieService zadowolenieService) {
+    public PromotionService(AltoService altoService, AmsoService amsoService, CarinetService carinetService, CombatService combatService, MoreleService moreleService, XkomService xkomService, ZadowolenieService zadowolenieService, SlackMessageSender slackMessageSender) {
         this.altoService = altoService;
         this.amsoService = amsoService;
         this.carinetService = carinetService;
@@ -27,6 +29,7 @@ public class PromotionService {
         this.moreleService = moreleService;
         this.xkomService = xkomService;
         this.zadowolenieService = zadowolenieService;
+        this.slackMessageSender = slackMessageSender;
     }
 
     public List<ProductDTO> getDailyPromotion() throws IOException {
@@ -38,7 +41,7 @@ public class PromotionService {
         productDTOList.add(moreleService.getPromotion());
         productDTOList.add(xkomService.getPromotion());
         productDTOList.add(zadowolenieService.getPromotion());
-
+        slackMessageSender.sendPromotionMessage(xkomService.getPromotion());
         return productDTOList.parallelStream().filter(Objects::nonNull).collect(Collectors.toList());
     }
 
