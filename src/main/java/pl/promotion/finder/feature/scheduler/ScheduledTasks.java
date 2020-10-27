c085a1ed39dbd7966a7a8d551674aa8e4a313ee4
Kamil Kafara <kafarakamil95@gmail.com>
2020-10-27 22:46:51 +0100

    change duration

--- src/main/java/pl/promotion/finder/feature/scheduler/ScheduledTasks.java
+++ src/main/java/pl/promotion/finder/feature/scheduler/ScheduledTasks.java
@@ -17,7 +17,7 @@ import java.util.Optional;
 @Component
 @Service
 public class ScheduledTasks {
-    private static final int DURATION = 15_000;
+    private static final int DURATION = 120_000;
     private final SlackMessageSender slackMessageSender;
     private final AmsoService amsoService;
     private final CarinetService carinetService;
