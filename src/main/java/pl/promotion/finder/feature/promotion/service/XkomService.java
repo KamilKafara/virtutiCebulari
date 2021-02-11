4579f8f2c5c3ba9d35cf623586257e2985c67686
Kamil Kafara <kafarakamil95@gmail.com>
2021-02-11 20:53:29 +0100

    fixup XkomService

--- src/main/java/pl/promotion/finder/feature/promotion/service/XkomService.java
+++ src/main/java/pl/promotion/finder/feature/promotion/service/XkomService.java
@@ -18,14 +18,12 @@ import java.io.IOException;
 @Service
 public class XkomService implements Promotion {
 
-    private static final String HOT_SHOT_TAG = "div.sc-bwzfXH.sc-1bb6kqq-2.cNKcdN.sc-htpNat.gSgMmi";
-    private static final String PROPERTY_TAG = "sc-1tblmgq-1 grqydx";
-    private static final String OLD_PRICE_TAG = "sc-1bb6kqq-4";
-    private static final String NEW_PRICE_TAG = "sc-1bb6kqq-5";
+    private static final String HOT_SHOT_TAG = "div.iRfMJZ";
+    private static final String PROPERTY_TAG = "div.hyIQof";
+    private static final String OLD_PRICE_TAG = "span.dNOsIM";
+    private static final String NEW_PRICE_TAG = "span.bxyEHo";
 
-    private static final String PRODUCT_NAME_TAG = "sc-1bb6kqq-10 kBnBfM m80syu-0 hGKlIY";
     private static final String AMOUNT_VALUE = "empty";
-    private static final String PRODUCT_IMAGE_TAG = "src";
     private static final String SHOP_NAME = "x-kom";
     private static final String SHOP_URL = "https://www.x-kom.pl/";
     private static final String PRODUCT_URL = "https://www.x-kom.pl/goracy_strzal";
@@ -43,13 +41,26 @@ public class XkomService implements Promotion {
     }
 
     public ProductDTO getProduct(Document document) {
-        Element element = document.select(HOT_SHOT_TAG).first();
+
+        Elements elements = document.select(HOT_SHOT_TAG);
+        if (elements.size() != 2) {
+            return null;
+        }
         ProductDTO productDTO = new ProductDTO(SHOP_NAME, PRODUCT_URL);
-        Elements productProperty = element.getElementsByClass(PROPERTY_TAG);
-        productDTO.setPictureUrl(productProperty.attr(PRODUCT_IMAGE_TAG));
-        productDTO.setProductName(element.getElementsByClass(PRODUCT_NAME_TAG).text());
-        String oldPrice = element.getElementsByClass(OLD_PRICE_TAG).text().replaceAll("zł", "").replaceAll("\\s+", "");
-        String newPrice = element.getElementsByClass(NEW_PRICE_TAG).text().replaceAll("zł", "").replaceAll("\\s+", "");
+
+        Element productInfo = elements.first();
+        Elements productProperty = productInfo.select("img.hyIQof");
+
+        String productImage = productProperty.attr("src");
+        String productName = productProperty.attr("alt");
+
+        productDTO.setPictureUrl(productImage);
+        productDTO.setProductName(productName);
+
+        productDTO.setAmount(AMOUNT_VALUE);
+
+        String oldPrice = elements.select(OLD_PRICE_TAG).text().replaceAll("zł", "").replaceAll("\\s+", "");
+        String newPrice = elements.select(NEW_PRICE_TAG).text().replaceAll("zł", "").replaceAll("\\s+", "");
 
         productDTO.setOldPrice(oldPrice);
         productDTO.setNewPrice(newPrice);
@@ -60,7 +71,6 @@ public class XkomService implements Promotion {
             productDTO.setOldPrice(String.format("%.2f", newPriceDouble) + " zł");
             productDTO.setNewPrice(String.format("%.2f", oldPriceDouble) + " zł");
         }
-        productDTO.setAmount(AMOUNT_VALUE);
         return productDTO;
     }
 
